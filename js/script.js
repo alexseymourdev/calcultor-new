ec9112f1222ef6a40f93cf894d7423caec4b542c
alexseymourdev <alexseymourdev@gmail.com>
2021-08-05 19:28:24 +0100

    adding pseudocode to JS file

--- js/script.js
+++ js/script.js
@@ -1,1 +1,61 @@
-console.log('test');
+// Number1, Number2, Operator
+
+// number1 {operator} number2
+
+// 10 + 5
+// 10 - 5
+// 10 / 5
+// 10 * 5
+
+/*-- 
+
+Cancel
+    number1 = "";
+    number2 = "";
+    operator = "";
+
+Numbers & Operators
+    IF number1 == ""
+        THEN number1 = numberPressed
+    ELSE 
+        IF operator == ""
+            THEN number1 += number
+
+    IF operator == "" AND number1 != ""
+        operator = operatorPressed
+    ELSE IF operator == ""  AND number1 == ""
+        THEN return error
+    ELSE IF operator != "" and number1 != "" AND number2 == ""
+        operator = operatorPressed
+    ELSE IF operator != "" AND number2 != ""
+        THEN number1 == sum(number1,number2,operator)
+        THEN operator == operatorPressed
+        THEN number2 == "";
+
+Equals Button
+    IF number1 != "" AND number2 !="" AND operator != ""
+        THEN sum = sum(number1,number2,operator);
+        THEN number1 = sum
+        THEN operator = ""
+        THEN number2 = "";
+
+Decimal
+    IF operator == ""
+        IF number1 == ""
+            THEN return "0."
+        IF number1 != ""
+            IF number1 has a decimal
+                THEN return error
+            ELSE
+                THEN append decimal to end of number1
+    ELSE
+        IF number2 == ""
+            THEN return "0."
+        IF number2 != ""
+            IF number2 has a decimal
+                THEN return error
+            ELSE
+                THEN append decimal to end of number2
+    
+    
+--*/
