e10abd3823ded024c730bebc699b39e88de5e8b0
alexseymourdev <alexseymourdev@gmail.com>
2021-08-10 20:26:34 +0100

    adding extra input for calculator display

--- js/script.js
+++ js/script.js
@@ -96,6 +96,7 @@ let objCalculator = {
             strMessage += ' '+this.number2;
         }
         this.objPreview.value = strMessage;
+        this.equals();
     },
     equals:function(){
         console.log('equals');
